a8f3ae7571f7bab96a0adf5df38c20f311d90bed
Branice Simaloi <simaloibranice@gmail.com>
2025-09-30 23:35:13 +0300

    Complete writeCards and countDown functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+// index.js
+
+// Assignment 1: writeCards()
+function writeCards(names, event) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+// Assignment 2: countDown()
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+// Export functions for testing
+module.exports = {
+  writeCards,
+  countDown,
+};
